5581cb748ed224a6fd69ed6b19c0414bfd991b7a
rv <1516081466@qq.com>
2024-03-13 22:14:35 +0800

    [add] 添加uart6与uart8驱动

--- LADRC_Board_NoneOS/User_Drivers/board_config.h
+++ LADRC_Board_NoneOS/User_Drivers/board_config.h
@@ -7,7 +7,9 @@
 
 #ifndef USER_DRIVERS_BOARD_CONFIG_H_
 #define USER_DRIVERS_BOARD_CONFIG_H_
+#include <uart6.h>
 #include <uart7.h>
+#include <uart8.h>
 #include "led.h"
 
 #endif /* USER_DRIVERS_BOARD_CONFIG_H_ */
---
+++ LADRC_Board_NoneOS/User_Drivers/uart8.h
@@ -0,0 +1,13 @@
+/*
+ * uart8.h
+ *
+ *  Created on: 2024Äê3ÔÂ13ÈÕ
+ *      Author: MCU
+ */
+
+#ifndef USER_DRIVERS_UART8_H_
+#define USER_DRIVERS_UART8_H_
+#include "ch32v30x.h"
+
+void UART8_GPIO_Init(void);
+#endif /* USER_DRIVERS_UART8_H_ */
