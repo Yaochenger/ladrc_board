18fc222f4fb376c08b94ec7211cdc2779b90f80b
rv <1516081466@qq.com>
2025-04-07 21:41:31 +0800

    Squashed commit of the following:
    commit c84dc90e13c7d76810ddf7007fc35a32a7b6d7c3
    Author: 1516081466@qq.com <1516081466@qq.com>
    Date:   Thu Mar 27 15:58:24 2025 +0800
    
        [add] 添加vofa+数据打印

---
+++ LADRC_Board_NoneOS/Board_Component/VOFA+/vofa.c
@@ -0,0 +1,41 @@
+#include "vofa.h"
+
+#define MAX_BUFFER_SIZE 512
+
+void send_to_vofa_fw(float *data, int num_channels)
+{
+    char buffer[MAX_BUFFER_SIZE];
+    int pos = 0;
+    const int max_write = MAX_BUFFER_SIZE - 2;
+
+    for (int i = 0; i < num_channels; i++) {
+
+        int written = snprintf(buffer + pos, max_write - pos, "%.2f", data[i]); // 返回格式化的字符串的长度 不包含\0
+
+        if (written < 0 || (pos + written) >= max_write) {
+            printf("Buffer overflow!\n"); //buffer yi'chu
+            return;
+        }
+        pos += written; // 设置写偏移
+
+        if (i < num_channels - 1) {
+            if (pos < max_write) {
+                buffer[pos++] = ','; // 添加数据分隔符
+            } else {
+                printf("Buffer overflow!\n");
+                return;
+            }
+        }
+    }
+
+    if (pos < MAX_BUFFER_SIZE - 1) { // 设置换行符
+        buffer[pos++] = '\n';
+        buffer[pos] = '\0';
+    }
+    else {
+        buffer[MAX_BUFFER_SIZE - 2] = '\n';
+        buffer[MAX_BUFFER_SIZE - 1] = '\0';
+    }
+
+    printf("%s", buffer);
+}
